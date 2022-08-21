fe196e94af1c3fb2adb975e18e8258b29d57c3be
Faisal Iqbal <faisaliqbal012@gmail.com>
2022-08-21 12:45:06 +0500

    social icons links added

--- src/components/social-icons.js
+++ src/components/social-icons.js
@@ -12,10 +12,14 @@ export default function SocialIcons() {
             <TiSocialLinkedin />
           </IconContext.Provider> */}
       <IconContext.Provider value={{ className: "social-icon" }}>
-        <IoLogoFacebook />
+        <a href="https://www.facebook.com/profile.php?id=100083154233339">
+          <IoLogoFacebook />
+        </a>
       </IconContext.Provider>
       <IconContext.Provider value={{ className: "social-icon" }}>
-        <TbBrandInstagram />
+        <a href="https://www.instagram.com/p/ChF_UhpsmnD/?igshid=YmMyMTA2M2Y=">
+          <TbBrandInstagram />
+        </a>
       </IconContext.Provider>
     </div>
   );
